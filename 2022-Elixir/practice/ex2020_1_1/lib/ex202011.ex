5767e0249cc82b41e31af2de7a8fe09e1c3f5b85
Kevin Barnes <iamkevb@gmail.com>
2022-11-26 10:16:24 -0500

    2020 Day 1 part 2

--- 2022-Elixir/practice/ex2020_1_1/lib/ex202011.ex
+++ 2022-Elixir/practice/ex2020_1_1/lib/ex202011.ex
@@ -7,23 +7,42 @@ defmodule Ex202011 do
     |> Enum.sort()
   end
 
-  def findProduct([]) do
+  def findProduct2(_, []) do
     0
   end
 
   # find 2 values that sum to 2020, return their product
-  def findProduct([head | tail]) do
-    v2 = Enum.find(tail, fn x -> 2020 - x === head end)
+  def findProduct2(sum, [head | tail]) do
+    v2 = Enum.find(tail, fn x -> sum - x === head end)
 
     if v2 != nil do
       v2 * head
     else
-      findProduct(tail)
+      findProduct2(sum, tail)
     end
   end
 
-  def run do
+  def findProduct3(_, []) do
+    0
+  end
+
+  def findProduct3(sum, [head | tail]) do
+    prod2 = findProduct2(sum - head, tail)
+
+    if prod2 > 0 do
+      head * prod2
+    else
+      findProduct3(sum, tail)
+    end
+  end
+
+  def part1 do
     values = readInput("input.txt")
-    findProduct(values)
+    findProduct2(2020, values)
+  end
+
+  def part2 do
+    values = readInput("input.txt")
+    findProduct3(2020, values)
   end
 end
