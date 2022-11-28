a2959b75d0734b46cff9d2599b813da0f5030d68
Kevin Barnes <iamkevb@gmail.com>
2022-11-28 07:26:50 -0500

    rename ex1 folder

---
+++ 2022-Elixir/practice/ex2020_1/lib/ex20201.ex
@@ -0,0 +1,47 @@
+defmodule Ex202011 do
+  def readInput(path) do
+    File.read!(path)
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.uniq()
+    |> Enum.sort()
+  end
+
+  def findProduct2(_, []) do
+    0
+  end
+
+  # find 2 values that sum to 2020, return their product
+  def findProduct2(sum, [head | tail]) do
+    v2 = Enum.find(tail, fn x -> sum - x === head end)
+
+    case v2 do
+      nil -> findProduct2(sum, tail)
+      _ -> v2 * head
+    end
+  end
+
+  def findProduct3(_, []) do
+    0
+  end
+
+  def findProduct3(sum, [head | tail]) do
+    prod2 = findProduct2(sum - head, tail)
+
+    if prod2 > 0 do
+      head * prod2
+    else
+      findProduct3(sum, tail)
+    end
+  end
+
+  def part1 do
+    values = readInput("input.txt")
+    findProduct2(2020, values)
+  end
+
+  def part2 do
+    values = readInput("input.txt")
+    findProduct3(2020, values)
+  end
+end
---
+++ 2022-Elixir/practice/ex2020_1/test/ex20201_test.exs
@@ -0,0 +1,8 @@
+defmodule Ex20201Test do
+  use ExUnit.Case
+  doctest Ex20201
+
+  test "greets the world" do
+    assert Ex20201.hello() == :world
+  end
+end
