4928f850f2de8e30032ef7293b633593de837c74
Kevin Barnes <iamkevb@gmail.com>
2022-12-02 10:00:26 -0500

    use functions and pattern matching the way functional gods intended

--- 2022-Elixir/day2/lib/day2.ex
+++ 2022-Elixir/day2/lib/day2.ex
@@ -10,61 +10,43 @@ defmodule Day2 do
   #  C Z: Scissors 3 points
 
   # Score Points for your move + (6 for win|3 for draw|0 for loss)
-
-  def score([opp, me]) do
-    case {opp, me} do
-      {"A", "X"} -> 1 + 3
-      {"A", "Y"} -> 2 + 6
-      {"A", "Z"} -> 3 + 0
-      {"B", "X"} -> 1 + 0
-      {"B", "Y"} -> 2 + 3
-      {"B", "Z"} -> 3 + 6
-      {"C", "X"} -> 1 + 6
-      {"C", "Y"} -> 2 + 0
-      {"C", "Z"} -> 3 + 3
-    end
-  end
+  defp score(["A", "X"]), do: 1 + 3
+  defp score(["A", "Y"]), do: 2 + 6
+  defp score(["A", "Z"]), do: 3 + 0
+  defp score(["B", "X"]), do: 1 + 0
+  defp score(["B", "Y"]), do: 2 + 3
+  defp score(["B", "Z"]), do: 3 + 6
+  defp score(["C", "X"]), do: 1 + 6
+  defp score(["C", "Y"]), do: 2 + 0
+  defp score(["C", "Z"]), do: 3 + 3
+  defp score([elf, player]), do: raise("oops #{elf} #{player}")
 
   # A,B,C: Rock, Paper, Scissors
   # X,Y,Z: Lose, Draw, Win
-  def moveAndScore([opp, result]) do
-    score2(result) + move({opp, result})
-  end
+  defp gameScore([_, "Y"]), do: 3
+  defp gameScore([_, "Z"]), do: 6
+  defp gameScore(_), do: 0
 
-  def score2(result) do
-    case result do
-      "Y" -> 3
-      "Z" -> 6
-      _ -> 0
-    end
-  end
-
-  def move({opp, result}) do
-    case {opp, result} do
-      # LOSE
-      {"A", "X"} -> 3
-      {"B", "X"} -> 1
-      {"C", "X"} -> 2
-      # DRAW
-      {"A", "Y"} -> 1
-      {"B", "Y"} -> 2
-      {"C", "Y"} -> 3
-      # WIN
-      {"A", "Z"} -> 2
-      {"B", "Z"} -> 3
-      {"C", "Z"} -> 1
-    end
-  end
+  defp moveScore(["A", "X"]), do: 3
+  defp moveScore(["A", "Y"]), do: 1
+  defp moveScore(["A", "Z"]), do: 2
+  defp moveScore(["B", "X"]), do: 1
+  defp moveScore(["B", "Y"]), do: 2
+  defp moveScore(["B", "Z"]), do: 3
+  defp moveScore(["C", "X"]), do: 2
+  defp moveScore(["C", "Y"]), do: 3
+  defp moveScore(["C", "Z"]), do: 1
+  defp moveScore([elf, player]), do: raise("oops #{elf} #{player}")
 
   def part1(path \\ "input.test.txt") do
     readInput(path)
-    |> Enum.map(&score/1)
-    |> Enum.sum()
+    |> Enum.reduce(0, fn e, acc -> acc + score(e) end)
   end
 
   def part2(path \\ "input.test.txt") do
     readInput(path)
-    |> Enum.map(&moveAndScore/1)
-    |> Enum.sum()
+    |> Enum.reduce(0, fn e, acc ->
+      acc + moveScore(e) + gameScore(e)
+    end)
   end
 end
