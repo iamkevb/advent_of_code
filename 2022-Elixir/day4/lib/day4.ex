9c0b5900da766a05606b591694d8aa91ab66a8b9
Kevin Barnes <iamkevb@gmail.com>
2022-12-04 08:32:56 -0500

    2022 - Day 4

---
+++ 2022-Elixir/day4/lib/day4.ex
@@ -0,0 +1,39 @@
+defmodule Day4 do
+  defp readFile(path) do
+    File.read!(path)
+    |> String.split("\n")
+  end
+
+  defp parseLine(l) do
+    Regex.scan(~r/(\d+)-(\d+),(\d+)-(\d+)/, l)
+    |> Enum.map(fn x -> tl(x) end)
+    |> List.flatten()
+    |> Enum.map(fn x -> String.to_integer(x) end)
+  end
+
+  defp overlapFull([a, b, c, d]), do: (a <= c && b >= d) || (c <= a && d >= b)
+  defp overlapFull(_), do: false
+
+  defp overlapAny([a, b, c, d]),
+    do:
+      (c <= a && a <= d) ||
+        (c <= b && b <= d) ||
+        (a <= c && c <= b) ||
+        (a <= d && d <= b)
+
+  defp overlapAny(_), do: false
+
+  def part1(path \\ "input.test.txt") do
+    readFile(path)
+    |> Enum.map(&parseLine/1)
+    |> Enum.filter(&overlapFull/1)
+    |> length()
+  end
+
+  def part2(path \\ "input.test.txt") do
+    readFile(path)
+    |> Enum.map(&parseLine/1)
+    |> Enum.filter(&overlapAny/1)
+    |> length()
+  end
+end
