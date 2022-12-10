2f374c0cea18a071ac14e2767d4110dff4a962ca
Kevin Barnes <iamkevb@gmail.com>
2022-12-10 09:15:51 -0500

    2022 - Day 10 - part2

--- 2022-Elixir/day10/lib/day10.ex
+++ 2022-Elixir/day10/lib/day10.ex
@@ -12,7 +12,6 @@ defmodule Day10 do
     |> Enum.reverse()
     |> List.to_tuple()
     |> then(fn l ->
-      #
       elem(l, 19) * 20 + elem(l, 59) * 60 + elem(l, 99) * 100 + elem(l, 139) * 140 +
         elem(l, 179) * 180 +
         elem(l, 219) * 220
@@ -22,7 +21,6 @@ defmodule Day10 do
   def handleInstruction(instruction, xValues) do
     [x | xValues] = xValues
     {num, val} = cycles(instruction)
-    # IO.puts("x:#{x} num:#{num} val:#{val} len xValues:#{length(xValues)}")
     [x + val | List.duplicate(x, num) ++ xValues]
   end
 
@@ -31,5 +29,25 @@ defmodule Day10 do
   def cycles(addx) do
     [_ | [c | _]] = String.split(addx)
     {2, String.to_integer(c)}
+  end
+
+  def part2(path \\ "input.test.txt") do
+    readInput(path)
+    |> Enum.reduce([1], fn i, acc ->
+      handleInstruction(i, acc)
+    end)
+    |> Enum.reverse()
+    |> Enum.with_index()
+    |> Enum.map(fn {x, idx} ->
+      pos = rem(idx, 40)
+
+      cond do
+        abs(x - pos) <= 1 -> "#"
+        true -> " "
+      end
+    end)
+    |> Enum.chunk_every(40)
+    |> Enum.map(&Enum.join/1)
+    |> Enum.each(&IO.puts/1)
   end
 end
